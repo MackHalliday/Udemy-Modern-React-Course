71759aebf3b2c14ab0d0222f60c132ffac5bf8af
mackhalliday <mack.halliday@15five.com>
2023-09-04 19:24:40 -0400

    Add form event handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,15 @@
-function SearchBar() {
-  return <div> SearchBar </div>;
+function SearchBar({ onSubmit }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("test!");
+  };
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
