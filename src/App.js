b70db785f49cc35536bb1cc38e33b90f06736530
mackhalliday <mack.halliday@15five.com>
2023-09-03 20:13:26 -0400

    Add description and columns

--- src/App.js
+++ src/App.js
@@ -7,14 +7,42 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <div>
-      <div> Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" imageSrc={AlexaImage} />
-      <ProfileCard
-        title="Cortana"
-        handle="@cortana32"
-        imageSrc={CortanaImage}
-      />
-      <ProfileCard title="Sira" handle="@siri01" imageSrc={SiriImage} />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                imageSrc={AlexaImage}
+                description="Amazon product"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                imageSrc={CortanaImage}
+                description="Microsoft product"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Sira"
+                handle="@siri01"
+                imageSrc={SiriImage}
+                description="Apple product"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
