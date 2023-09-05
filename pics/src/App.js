f70b2f0f601b907059954d2b4b92fac52615ff9d
mackhalliday <mack.halliday@15five.com>
2023-09-04 21:20:08 -0400

    Complete search pic app

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,15 +1,20 @@
 import SearchBar from "./components/SearchBar";
 import searchImages from "./api.js";
+import ImageList from "./components/ImageList";
+import { useState } from "react";
 
 function App() {
+  const [images, setImages] = useState([]);
+
   const handleSearch = async (term) => {
     const result = await searchImages(term);
-    console.log(result);
+    setImages(result);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
+      <ImageList images={images} />
     </div>
   );
 }
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,15 @@
-function ImageList() {
-  return <div> ImageList </div>;
+import "./ImageList.css";
+import ImageShow from "./ImageShow";
+
+function ImageList({ images }) {
+  const renderImages = images.map((image) => {
+    return (
+      <div key={image.id}>
+        <ImageShow image={image} />
+      </div>
+    );
+  });
+  return <div className="image-list"> {renderImages} </div>;
 }
 
 export default ImageList;
