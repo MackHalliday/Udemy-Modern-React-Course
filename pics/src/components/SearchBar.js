f113c495f6d7fbf1418e7d7aa28b3926eb7a61c9
mackhalliday <mack.halliday@15five.com>
2023-09-04 20:34:55 -0400

    Add ability to handle change input

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("default text!");
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    console.log("test!");
+    onSubmit(term);
   };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input />
+        <input value={term} onChange={handleChange} />
       </form>
     </div>
   );
