1e190a562bcf03c2763d41fc0a5c79735e952a4d
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-28 11:49:56 -0500

    Can now update book title for specific book using new updateBookById method

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,5 +1,30 @@
-function BookEdit() {
-    return <div> Book Edit</div>
+import { useState } from "react";
+
+function BookEdit({ title, id, editBook }) {
+  const [newTitle, setNewTitle] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    editBook(newTitle, id);
+    setNewTitle("");
+  };
+
+  return (
+    <div className="book-edit" id={id}>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter new title"
+          value={newTitle}
+          onChange={(event) => setNewTitle(event.target.value)}
+        />
+        <button className="buttom" type="submit">
+          Edit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default BookEdit
+export default BookEdit;
