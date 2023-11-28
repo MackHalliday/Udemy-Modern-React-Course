89999029afd5721ba2529975b5751e6a4a765c5d
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-28 11:50:51 -0500

    Add BookEdit method to BookShow method

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,7 +1,10 @@
-function BookShow({ title, index }) {
+import BookEdit from "./BookEdit";
+
+function BookShow({ title, id, editBook }) {
   return (
-    <div className="book-show" key={index}>
+    <div className="book-show" id={id}>
       <h2>{title}</h2>
+      <BookEdit title={title} id={id} editBook={editBook} />
     </div>
   );
 }
