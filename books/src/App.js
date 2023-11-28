7fb4113f64c43d3fcf6ee5a6fc1e77daacfd60e4
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-28 11:49:09 -0500

    Change index to id and add ability to update book by id. Passed new updateBookById method into BookList component

--- books/src/App.js
+++ books/src/App.js
@@ -3,16 +3,30 @@ import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
-  const [books, setBooks] = useState([{title:"Chicken", index: 1}]);
+  const [books, setBooks] = useState([{ title: "Chicken", id: 1 }]);
 
   const createBook = (title) => {
-    let numBooks = books.length
-    setBooks([...books, {title:title, index:(numBooks+1)}]);
+    let numBooks = books.length;
+    setBooks([...books, { title: title, id: numBooks + 1 }]);
+  };
+
+  const updateBookById = (newTitle, id) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
   };
 
   return (
     <div>
-      <div> <BookList books={books}/></div>
+      <div>
+        {" "}
+        <BookList books={books} editBook={updateBookById} />
+      </div>
       <div>
         <BookCreate onCreate={createBook} />
       </div>
