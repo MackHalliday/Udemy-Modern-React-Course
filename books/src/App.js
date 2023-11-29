70b3e51ec2390edc70666e78b8dcac74d988a650
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-28 20:46:03 -0500

    Add delete book function, add indexing

--- books/src/App.js
+++ books/src/App.js
@@ -3,11 +3,13 @@ import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
-  const [books, setBooks] = useState([{ title: "Chicken", id: 1 }]);
+  const [books, setBooks] = useState([{ title: "Test Title 1", id: 1 }]);
+  const [numBooks, setNumBooks] = useState(1);
 
   const createBook = (title) => {
-    let numBooks = books.length;
-    setBooks([...books, { title: title, id: numBooks + 1 }]);
+    let nextBookId = numBooks + 1;
+    setBooks([...books, { title: title, id: nextBookId }]);
+    setNumBooks(nextBookId);
   };
 
   const updateBookById = (newTitle, id) => {
@@ -21,10 +23,17 @@ function App() {
     setBooks(updatedBooks);
   };
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return (book.id !== id) 
+    });
+    setBooks(updatedBooks)
+  }
+
   return (
-    <div>
+    <div className="app">
       <div>
-        <BookList books={books} editBook={updateBookById} />
+        <BookList books={books} onEdit={updateBookById} onDelete={deleteBookById} />
       </div>
       <div>
         <BookCreate onCreate={createBook} />
